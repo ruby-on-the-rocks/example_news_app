93c4d0377f4b494ba4c599c2ef43691c6a761bdc
Peter Jang <peter.x.jang@gmail.com>
2019-03-18 21:31:27 -0500

    Secure API key using figaro gem

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,6 +1,6 @@
 class Api::ArticlesController < ApplicationController
   def index
-    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&apiKey=YOUR-API-KEY-HERE")
+    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&apiKey=#{ENV["NEWS_API_KEY"]}")
     @articles = response.parse["articles"]
     render "index.json.jbuilder"
   end
