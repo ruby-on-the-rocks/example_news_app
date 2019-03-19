bf1508c2ecd9d19f11bfadbdbab7c0344b1a5888
Peter Jang <peter.x.jang@gmail.com>
2019-03-18 21:06:29 -0500

    Add articles index action using news api

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,2 +1,7 @@
 class Api::ArticlesController < ApplicationController
+  def index
+    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&apiKey=YOUR-API-KEY-HERE")
+    @articles = response.parse["articles"]
+    render "index.json.jbuilder"
+  end
 end
